c4259253fc4ef457810c2a070a5d229a3b9d410e
umar006 <umaralfaruq597@gmail.com>
2024-06-20 16:00:04 +0700

    feat: task schema

---
+++ src/task/task.enum.ts
@@ -0,0 +1,11 @@
+export enum TaskPriorityEnum {
+  LOW = 'low',
+  MEDIUM = 'medium',
+  HIGH = 'high',
+}
+
+export enum TaskStatusEnum {
+  TODO = 'todo',
+  INPROGRESS = 'in-progress',
+  COMPLETED = 'completed',
+}
---
+++ src/task/task.schema.ts
@@ -0,0 +1,32 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument, now } from 'mongoose';
+import { v4 as uuidv4 } from 'uuid';
+import { TaskPriorityEnum, TaskStatusEnum } from './task.enum';
+
+export type TaskDocument = HydratedDocument<Task>;
+
+@Schema()
+export class Task {
+  @Prop({ required: true, default: uuidv4 })
+  id: string;
+
+  @Prop({ required: true, maxlength: 100 })
+  title: string;
+
+  @Prop({ maxlength: 1000 })
+  description?: string;
+
+  @Prop({ default: now })
+  dueDate?: Date;
+
+  @Prop({ type: String, enum: TaskPriorityEnum })
+  priority?: TaskPriorityEnum;
+
+  @Prop({ required: true, type: String, enum: TaskStatusEnum })
+  status: TaskStatusEnum;
+
+  @Prop({ type: [{ type: String, maxlength: 50 }] })
+  tags?: string[];
+}
+
+export const TaskSchema = SchemaFactory.createForClass(Task);
