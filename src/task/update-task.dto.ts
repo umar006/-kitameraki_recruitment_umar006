a025f57de8b8260abe4034b1dca3776e7c8f5c7a
umar006 <umaralfaruq597@gmail.com>
2024-06-20 22:03:39 +0700

    feat: udpate task dto

---
+++ src/task/update-task.dto.ts
@@ -0,0 +1,41 @@
+import {
+  IsDateString,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator';
+import { TaskPriorityEnum, TaskStatusEnum } from './task.enum';
+
+export class UpdateTaskDto {
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
+  title?: string;
+
+  @IsOptional()
+  @IsNotEmpty()
+  @IsEnum(TaskStatusEnum)
+  status?: TaskStatusEnum;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(1000)
+  description?: string;
+
+  @IsOptional()
+  @IsDateString()
+  dueDate?: Date;
+
+  @IsOptional()
+  @IsEnum(TaskPriorityEnum)
+  priority?: TaskPriorityEnum;
+
+  @IsOptional()
+  @IsString({ each: true })
+  @IsNotEmpty({ each: true })
+  @MaxLength(50, { each: true })
+  tags?: string[];
+}
