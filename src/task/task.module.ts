476704c52c12bad1bbc6887d81d737e41619a800
umar006 <umaralfaruq597@gmail.com>
2024-06-21 08:32:01 +0700

    feat: task repository

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { TaskController } from './task.controller';
+import { TaskRepository } from './task.repository';
 import { Task, TaskSchema } from './task.schema';
 import { TaskService } from './task.service';
-import { TaskController } from './task.controller';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Task.name, schema: TaskSchema }]),
   ],
-  providers: [TaskService],
+  providers: [TaskService, TaskRepository],
   controllers: [TaskController],
 })
 export class TaskModule {}
