4c4200f4db4795c7cc51483f8f18c7bbfaf7c9ab
umar006 <umaralfaruq597@gmail.com>
2024-06-21 14:20:29 +0700

    feat: apply helmet security headers

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,7 @@ async function bootstrap() {
   });
 
   app.enableCors();
+  app.use(helmet());
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
