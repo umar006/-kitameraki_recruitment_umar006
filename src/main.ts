5ace0860e6fe94a27799dc00bf8f243bd92bab12
umar006 <umaralfaruq597@gmail.com>
2024-06-21 14:19:08 +0700

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,14 @@ import { ValidationPipe, VersioningType } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
-
   app.enableVersioning({
     type: VersioningType.URI,
     defaultVersion: '1',
   });
+
+  app.enableCors();
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   await app.listen(3000);
 }
