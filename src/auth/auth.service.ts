df3d1e635da968b9bafd64c4c5de0a09e4684755
umar006 <umaralfaruq597@gmail.com>
2024-06-21 22:25:23 +0700

    refactor: hash password before saved to db

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { RegisterDto } from './dto/register.dto';
 import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly userService: UserService) {}
 
   async register(registerDto: RegisterDto): Promise<void> {
-    await this.userService.createUser(registerDto);
+    const hashedPwd = await bcrypt.hash(registerDto.password, 10);
+
+    await this.userService.createUser({
+      email: registerDto.email,
+      password: hashedPwd,
+    });
   }
 }
