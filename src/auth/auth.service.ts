abebc158affda889302deee064bfca1964842574
umar006 <umaralfaruq597@gmail.com>
2024-06-21 22:44:31 +0700

    refactor: move login to createUser method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,4 @@
-import {
-  Injectable,
-  InternalServerErrorException,
-  UnprocessableEntityException,
-} from '@nestjs/common';
-import * as bcrypt from 'bcrypt';
+import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { RegisterDto } from './dto/register.dto';
 
@@ -12,19 +7,6 @@ export class AuthService {
   constructor(private readonly userService: UserService) {}
 
   async register(registerDto: RegisterDto): Promise<void> {
-    const hashedPwd = await bcrypt.hash(registerDto.password, 10);
-
-    try {
-      await this.userService.createUser({
-        email: registerDto.email,
-        password: hashedPwd,
-      });
-    } catch (err) {
-      console.error(err);
-      if (err.code !== null && err.code === 11000) {
-        throw new UnprocessableEntityException('email already exists');
-      }
-      throw new InternalServerErrorException();
-    }
+    await this.userService.createUser(registerDto);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,33 @@
-import { Injectable } from '@nestjs/common';
-import { UserRepository } from './user.repository';
+import {
+  Injectable,
+  InternalServerErrorException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './schema/user.schema';
+import { UserRepository } from './user.repository';
 
 @Injectable()
 export class UserService {
   constructor(private readonly userRepository: UserRepository) {}
 
-  createUser(createUserDto: CreateUserDto): Promise<User> {
-    return this.userRepository.createUser(createUserDto);
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const hashedPwd = await bcrypt.hash(createUserDto.password, 10);
+
+    try {
+      const createdUser = await this.userRepository.createUser({
+        ...createUserDto,
+        password: hashedPwd,
+      });
+
+      return createdUser;
+    } catch (err) {
+      console.error(err);
+      if (err.code !== null && err.code === 11000) {
+        throw new UnprocessableEntityException('email already exists');
+      }
+      throw new InternalServerErrorException();
+    }
   }
 }
