fdc460977974183add9b4cccc26dbb061ce4a051
umar006 <umaralfaruq597@gmail.com>
2024-06-21 22:21:47 +0700

    feat: register api

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schema/user.schema';
+import { UserRepository } from './user.repository';
+import { UserService } from './user.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
+  providers: [UserService, UserRepository],
+  exports: [UserService],
 })
 export class UserModule {}
---
+++ src/user/user.repository.ts
@@ -0,0 +1,15 @@
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserMapper } from './mapper/user.mapper';
+import { User } from './schema/user.schema';
+
+export class UserRepository {
+  constructor(@InjectModel(User.name) private taskModel: Model<User>) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const createdUser = new this.taskModel(createUserDto);
+    const userObject = await createdUser.save();
+    return UserMapper.toDomain(userObject);
+  }
+}
