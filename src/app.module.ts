19577349875c7f768636d3114f6318e5491a2195
umar006 <umaralfaruq597@gmail.com>
2024-06-21 21:46:00 +0700

    feat: user schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import authConfig from './auth/auth.config';
+import { AuthModule } from './auth/auth.module';
 import databaseConfig from './database/database.config';
 import { DatabaseModule } from './database/database.module';
 import { TaskModule } from './task/task.module';
-import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { AuthModule } from './auth/auth.module';
     DatabaseModule,
     TaskModule,
     AuthModule,
+    UserModule,
   ],
   controllers: [],
   providers: [],
