488f90181a06fa78b645bcee3376a7018c456d4f
umar006 <umaralfaruq597@gmail.com>
2024-06-22 09:24:45 +0700

    feat: custom swagger decorator login api

--- src/auth/swagger/custom-decorator.swagger.ts
+++ src/auth/swagger/custom-decorator.swagger.ts
@@ -1,12 +1,25 @@
 import { applyDecorators } from '@nestjs/common';
 import {
   ApiNoContentResponse,
+  ApiOkResponse,
+  ApiUnauthorizedResponse,
   ApiUnprocessableEntityResponse,
 } from '@nestjs/swagger';
+import { LoginResponseDto } from '../dto/login-response.dto';
 
 export const ApiRegisterResponse = () => {
   return applyDecorators(
     ApiNoContentResponse({ description: 'Success register user' }),
     ApiUnprocessableEntityResponse({ description: 'Unprocessable Entity' }),
+  );
+};
+
+export const ApiLoginResponse = () => {
+  return applyDecorators(
+    ApiOkResponse({
+      type: LoginResponseDto,
+      description: 'Success login user',
+    }),
+    ApiUnauthorizedResponse({ description: 'Unauthorized' }),
   );
 };
