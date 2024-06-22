4cf25215e523d06a6efcf5aedd7ebff93e6290c5
umar006 <umaralfaruq597@gmail.com>
2024-06-22 09:24:45 +0700

    feat: custom swagger decorator login api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,7 +4,10 @@ import { AuthService } from './auth.service';
 import { LoginResponseDto } from './dto/login-response.dto';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
-import { ApiRegisterResponse } from './swagger/custom-decorator.swagger';
+import {
+  ApiLoginResponse,
+  ApiRegisterResponse,
+} from './swagger/custom-decorator.swagger';
 
 @ApiTags('auth')
 @Controller('auth')
@@ -18,6 +21,7 @@ export class AuthController {
     return this.authService.register(registerDto);
   }
 
+  @ApiLoginResponse()
   @Post('login')
   @HttpCode(HttpStatus.OK)
   async login(@Body() loginDto: LoginDto): Promise<LoginResponseDto> {
