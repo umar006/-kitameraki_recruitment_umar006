693116f93f215c686ca730b992082bba3ce4615b
umar006 <umaralfaruq597@gmail.com>
2024-06-22 10:33:29 +0700

    feat: jwt guard

---
+++ src/auth/guard/jwt.guard.ts
@@ -0,0 +1,36 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+
+@Injectable()
+export class JwtGuard implements CanActivate {
+  constructor(private readonly jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest<Request>();
+    const token = this.extractTokenFromCookie(request);
+    if (!token) {
+      throw new UnauthorizedException('you have not login');
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token);
+      request['user'] = payload;
+    } catch (err) {
+      console.error(err);
+      throw new UnauthorizedException('jwt malformed or expired');
+    }
+
+    return true;
+  }
+
+  private extractTokenFromCookie(request: Request): string | null {
+    const token = request.cookies['Authentication'];
+    return token ? token : null;
+  }
+}
