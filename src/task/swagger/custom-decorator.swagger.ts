df0478e35525a2f176df8523303d9d5df704a957
umar006 <umaralfaruq597@gmail.com>
2024-06-23 11:14:37 +0700

    feat: swagger description to tasks api

--- src/task/swagger/custom-decorator.swagger.ts
+++ src/task/swagger/custom-decorator.swagger.ts
@@ -5,11 +5,13 @@ import {
   ApiNoContentResponse,
   ApiNotFoundResponse,
   ApiOkResponse,
+  ApiOperation,
 } from '@nestjs/swagger';
 import { Task } from '../schema/task.schema';
 
 export const ApiCreateTaskResponse = () => {
   return applyDecorators(
+    ApiOperation({ summary: 'Create a new task' }),
     ApiCreatedResponse({
       type: Task,
       description: 'Success create a new task',
@@ -20,6 +22,7 @@ export const ApiCreateTaskResponse = () => {
 
 export const ApiGetAllTasksResponse = () => {
   return applyDecorators(
+    ApiOperation({ summary: 'Gell all tasks' }),
     ApiOkResponse({
       type: Task,
       isArray: true,
@@ -30,6 +33,7 @@ export const ApiGetAllTasksResponse = () => {
 
 export const ApiGetTaskByIdResponse = () => {
   return applyDecorators(
+    ApiOperation({ summary: 'Get a task by id' }),
     ApiOkResponse({ type: Task, description: 'Success get task' }),
     ApiNotFoundResponse({ description: 'Task Not Found' }),
   );
@@ -37,6 +41,7 @@ export const ApiGetTaskByIdResponse = () => {
 
 export const ApiUpdateTaskByIdResponse = () => {
   return applyDecorators(
+    ApiOperation({ summary: 'Update a task by id' }),
     ApiOkResponse({ type: Task, description: 'Success update task' }),
     ApiBadRequestResponse({ description: 'Validation Error' }),
     ApiNotFoundResponse({ description: 'Task Not Found' }),
@@ -45,6 +50,7 @@ export const ApiUpdateTaskByIdResponse = () => {
 
 export const ApiDeleteTaskByIdResponse = () => {
   return applyDecorators(
+    ApiOperation({ summary: 'Delete a task by id' }),
     ApiNoContentResponse({ description: 'Success delete a task' }),
     ApiNotFoundResponse({ description: 'Task Not Found' }),
   );
