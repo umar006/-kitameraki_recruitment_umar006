eacbb4ac1c25ef62a7fdb79395a8fdffdf0e9790
umar006 <umaralfaruq597@gmail.com>
2024-06-24 11:35:18 +0700

    feat: task type

--- frontend/src/types/task.ts
+++ frontend/src/types/task.ts
@@ -1,3 +1,13 @@
+export type Task = {
+  id: string;
+  title: string;
+  status: string;
+  description: string;
+  dueDate: string;
+  priority: string;
+  tags: string[];
+};
+
 export type AddTask = {
   title?: string;
   description?: string;
