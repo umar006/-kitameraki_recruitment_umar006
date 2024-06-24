bfd5f1f27988c875ab67cdf459c2a0ea6e2e85c1
umar006 <umaralfaruq597@gmail.com>
2024-06-24 07:51:25 +0700

    feat: setup tanstack query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,15 @@
 import { initializeIcons } from '@fluentui/react/lib/Icons';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
 initializeIcons();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>,
+);
