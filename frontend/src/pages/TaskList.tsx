a70d999db2538bbb4aebb578624dfde2273e3946
umar006 <umaralfaruq597@gmail.com>
2024-06-25 12:30:14 +0700

    fix: query invalidation after delete task

--- frontend/src/pages/TaskList.tsx
+++ frontend/src/pages/TaskList.tsx
@@ -1,26 +1,26 @@
-import { useBoolean } from '@fluentui/react-hooks';
-import { PrimaryButton } from '@fluentui/react/lib/Button';
-import { FocusTrapZone } from '@fluentui/react/lib/FocusTrapZone';
-import { Layer } from '@fluentui/react/lib/Layer';
-import { List } from '@fluentui/react/lib/List';
-import { Overlay } from '@fluentui/react/lib/Overlay';
-import { Popup } from '@fluentui/react/lib/Popup';
-import { IStackTokens, Stack } from '@fluentui/react/lib/Stack';
-import { ITheme, mergeStyleSets, normalize } from '@fluentui/react/lib/Styling';
-import { useTheme } from '@fluentui/react/lib/Theme';
+import { useBoolean } from "@fluentui/react-hooks";
+import { PrimaryButton } from "@fluentui/react/lib/Button";
+import { FocusTrapZone } from "@fluentui/react/lib/FocusTrapZone";
+import { Layer } from "@fluentui/react/lib/Layer";
+import { List } from "@fluentui/react/lib/List";
+import { Overlay } from "@fluentui/react/lib/Overlay";
+import { Popup } from "@fluentui/react/lib/Popup";
+import { IStackTokens, Stack } from "@fluentui/react/lib/Stack";
+import { ITheme, mergeStyleSets, normalize } from "@fluentui/react/lib/Styling";
+import { useTheme } from "@fluentui/react/lib/Theme";
 import {
   useInfiniteQuery,
   useMutation,
   useQueryClient,
-} from '@tanstack/react-query';
-import { useEffect, useState } from 'react';
-import { useInView } from 'react-intersection-observer';
+} from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
 import {
   deleteTaskById,
   getTaskListInfiniteQuery,
-} from '../services/taskServices';
-import { Task } from '../types/task';
-import UpdateTaskForm from './UpdateTaskForm';
+} from "../services/taskServices";
+import { Task } from "../types/task";
+import UpdateTaskForm from "./UpdateTaskForm";
 
 export default function TaskList() {
   const [isPopupVisible, { setTrue: showPopup, setFalse: hidePopup }] =
@@ -29,7 +29,7 @@ export default function TaskList() {
   const theme = useTheme();
 
   const { data, isLoading, fetchNextPage } = useInfiniteQuery({
-    queryKey: ['tasks'],
+    queryKey: ["tasks"],
     queryFn: getTaskListInfiniteQuery,
     initialPageParam: 1,
     getNextPageParam: (lastPage) => lastPage.nextPage,
@@ -47,14 +47,12 @@ export default function TaskList() {
 
   const deleteTaskMutation = useMutation({
     mutationFn: deleteTaskById,
-    onSuccess: (_: void, deleteTask: string) => {
-      queryClient.setQueryData(['tasks'], (oldData: Task[]) => {
-        return oldData.filter((todo) => todo.id !== deleteTask);
-      });
+    onSuccess: () => {
+      queryClient.invalidateQueries();
     },
   });
 
-  if (isLoading) return 'Loading...';
+  if (isLoading) return "Loading...";
 
   const onRenderCell = (task?: Task) => {
     return (
@@ -111,29 +109,29 @@ export default function TaskList() {
 
 const popupStyles = mergeStyleSets({
   root: {
-    background: 'rgba(0, 0, 0, 0.2)',
-    bottom: '0',
-    left: '0',
-    position: 'fixed',
-    right: '0',
-    top: '0',
+    background: "rgba(0, 0, 0, 0.2)",
+    bottom: "0",
+    left: "0",
+    position: "fixed",
+    right: "0",
+    top: "0",
   },
 });
 
 const generateStyles = (theme: ITheme) => {
   return mergeStyleSets({
     container: {
-      overflow: 'auto',
+      overflow: "auto",
       maxWidth: 512,
-      border: '1px solid ' + theme.palette.neutralLight,
+      border: "1px solid " + theme.palette.neutralLight,
       marginTop: 24,
       selectors: {
-        '.ms-List-cell': {
+        ".ms-List-cell": {
           height: 52,
           lineHeight: 52,
-          textOverflow: 'clip',
+          textOverflow: "clip",
           background: theme.palette.neutralLighter,
-          borderBottom: '2px solid white',
+          borderBottom: "2px solid white",
         },
       },
     },
@@ -142,9 +140,9 @@ const generateStyles = (theme: ITheme) => {
       {
         paddingLeft: 16,
         paddingRight: 16,
-        whiteSpace: 'wrap',
-        overflow: 'hidden',
-        textOverflow: 'ellipsis',
+        whiteSpace: "wrap",
+        overflow: "hidden",
+        textOverflow: "ellipsis",
       },
     ],
   });
